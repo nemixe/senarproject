f5cbdae23ab2de3bc9f743faa4890bce8c9556d2
nemixe <rafel.ami@gmail.com>
2018-04-12 06:55:44 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="ui menu">
+          <div className="ui fluid container">
+            <div className="ui item">
+              Home
+            </div>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default App;
