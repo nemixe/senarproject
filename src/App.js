d2520d8283cbafde4ba61142f01966e4200db196
nemixe <rafel.ami@gmail.com>
2018-04-12 07:33:05 +0700

    semantic

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-        <div className="ui menu">
-          <div className="ui fluid container">
-            <div className="ui item">
-              Home
-            </div>
+      <div class="ui secondary menu">
+      <a class="item active">
+        Home
+      </a>
+      <a class="item">
+        Messages
+      </a>
+      <a class="item">
+        Friends
+      </a>
+      <div class="right menu">
+        <div class="item">
+          <div class="ui icon input">
+            <input type="text" placeholder="Search..."/>
+            <i class="search link icon"></i>
           </div>
         </div>
+        <a class="ui item">
+          Logout
+        </a>
+      </div>
+</div>
     );
   }
 }
